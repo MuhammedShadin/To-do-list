646fbcc73ddb7e2239d75973ed1646df95bc6506
Muhammed Shadin <muhammedshadinlf@gmail.com>
2025-04-30 21:05:33 +0530

    third

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 var task = document.getElementById("things").value;
-var button = document.getElementById("add").value;
+var button = document.getElementById("add");
+button.onclick=handle;
+function handle(){
+    var task = document.getElementById("things").value;
+    
+}
